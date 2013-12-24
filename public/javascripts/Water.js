2b1cc0baa6ffd4667a9723ca185183d09028fe01
Vinodkumar Subramaney <vsubramaney@corp.ebay.com>
2013-12-24 17:26:02 +0530

    fixed the water showup

---
+++ public/javascripts/Water.js
@@ -0,0 +1,9 @@
+/**
+ * Created by vsubramaney on 12/24/13.
+ */
+
+window.onload = function(){
+    var width = document.body.width;
+    var height = document.body.height;
+    document.getElementById("WaterVideo").style.width = 1700;
+}
